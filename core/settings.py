07e81878a1894040b49d2ebab1e232dcc0cd9b09
Sergio Tavares <sergio.ozorio32@gmail.com>
2024-07-17 20:30:23 -0300

    Template Base

--- core/settings.py
+++ core/settings.py
@@ -10,34 +10,85 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
+import os 
+import sys
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
+
+
+# Adicionar essa tag para que nosso projeto encontre o .env
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+
+# Diz para Django onde estão nossos aplicativos
+APPS_DIR = str(os.path.join(BASE_DIR,'apps'))  
+sys.path.insert(0, APPS_DIR)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&wl05^eod6ua=pck*14(w8$^*6_lafv*mv=a3q-jrsy1t-593u'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',  
+    '127.0.0.1',
+]
+
+CORS_ALLOW_HEADERS = list(default_headers) + [  
+    'X-Register',  
+]
+
+# CORS Config
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL como True, o que permite que qualquer site acesse seus recursos.
+# Defina como False e adicione o site no CORS_ORIGIN_WHITELIST onde somente o site da lista acesse os seus recursos.
+CORS_ALLOW_CREDENTIALS = False
+
+CORS_ORIGIN_WHITELIST = ['http://meusite.com',] # Lista.
+
+
+if not DEBUG:  
+SECURE_SSL_REDIRECT = True  
+ADMINS = [(os.getenv('SUPER_USER'), os.getenv('EMAIL'))]  
+SESSION_COOKIE_SECURE = True  
+CSRF_COOKIE_SECURE = True
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
+
+
+THIRD_APPS = [  
+...  
+]  
+
+
+PROJECT_APPS = [  
+'apps.base',  
+# 'apps.myapp',  
+
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + PROJECT_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +98,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
+    'django.middleware.common.CommonMiddleware',
+    'requestlogs.middleware.RequestLogsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -75,8 +129,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        ENGINE': 'django.db.backends.sqlite3',  
+        'NAME': os.path.join(BASE_DIR, os.getenv('NAME_DB')),  
+            #'USER':os.getenv('USER_DB')  
+            #'PASSWORD': os.getenv('PASSWORD_DB')  
+            #'HOST':os.getenv('HOST_DB')  
+            #'PORT':os.getenv('PORT_DB')
     }
 }
 
@@ -99,15 +157,45 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK={  
+... 'EXCEPTION_HANDLER': 'requestlogs.views.exception_handler',  
+}
+
+
+LOGGING = {  
+    'version': 1,  
+    'disable_existing_loggers': False,  
+    'handlers': {  
+        'requestlogs_to_file': {  
+            'level': 'INFO',  
+            'class': 'logging.FileHandler',  
+            'filename': 'info.log',  
+        },  
+},  
+
+    'loggers': {  
+        'requestlogs': {  
+            'handlers': ['requestlogs_to_file'],  
+            'level': 'INFO',  
+            'propagate': False,  
+        },  
+    },  
+}
+
+REQUESTLOGS = {  
+    'SECRETS': ['password', 'token'],  
+    'METHODS': ('PUT', 'PATCH', 'POST', 'DELETE'),  
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
@@ -115,9 +203,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/' 
+
+
+# STATICFILES_DIRS = [ # talvez em Produção podesse usar assim.
+#     BASE_DIR / 'static',
+# ]
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Se tiver configuração de email
+EMAIL_HOST = os.getenv('EMAIL_HOST')  
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
+EMAIL_PORT = os.getenv('EMAIL_PORT')  
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')  
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')  
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
